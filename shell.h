cdd6c68211c5fbc742f777520985e282a7bcddb5
root <Joshua-kiage>
2023-05-17 12:44:57 -0700

    shell.h file

---
+++ shell.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stddef.h>
+#include <string.h>
+
+void execute_command(const char* command);
+
+#endif
